7464398ead522fcbe185e1409ca223899b85f88b
BugHunter-X <askzenai@gmail.com>
2025-01-23 20:40:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Some Tailwind CSS classes are not applied correctly.
+/* Example */
+<div class="bg-red-500 p-4">
+  <p>This text should have a red background and padding.</p>
+</div>
+```
